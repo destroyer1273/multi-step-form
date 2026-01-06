4ead8c10544f1e24bb51a48d832dd0d1e07f18bb
Matvey <rofl.1991@list.ru>
2026-01-06 23:36:04 +0700

    add 2 steps of form

--- multistepform/src/views/FormView.vue
+++ multistepform/src/views/FormView.vue
@@ -1,8 +1,11 @@
 <script setup>
+    import { ref } from "vue";
 // IMAGES IMAGES IMAGES IMAGES IMAGES
 // import leftBgImage from "@/assets/images/bg-sidebar-desktop.svg";
 const leftBgImage = '/images/bg-sidebar-desktop.svg';
 // IMAGES IMAGES IMAGES IMAGES IMAGES
+
+const currentStep = ref(1);
 </script>
 
 <template>
@@ -41,18 +44,92 @@ const leftBgImage = '/images/bg-sidebar-desktop.svg';
     </div>
     <div class="right-form">
 
+        <!-- <div class="content1">
+            <h1>Personal info</h1>
+            <p>Please provide your name, email address, and phone number.</p>
+            <form class="form">
+                <label for="name">Name</label>
+                <input type="text" placeholder="name" id="name">
+                <label for="email">Email address</label>
+                <input type="text" placeholder="email" id="email">
+                <label for="phone">Phone Number</label>
+                <input type="text" placeholder="+79921229322" id="phone">
+                <button>Next step</button>
+            </form>
+        </div> -->
+        <div class="content2">
+            <h1>Select your plan</h1>
+            <p class="content2-titlep">You have the option of monthly or yearly billing</p>
+            <div class="select-div">
+                <div class="select-item">
+                    <img src="/images/icon-arcade.svg" alt="img" class="select-img"> 
+                    <h3>Arcade</h3>
+                    <p>$9/mo</p>
+                </div>
+                <div class="select-item">
+                    <img src="/images/icon-advanced.svg" alt="img" class="select-img">
+                    <h3>Advanced</h3>
+                    <p>$12/mo</p>
+                </div>
+                <div class="select-item">
+                    <img src="/images/icon-pro.svg" alt="img" class="select-img">
+                    <h3>Pro</h3>
+                    <p>$15/mo</p>
+                </div>
+            </div>
+            <div class="check">
+                <p>Monthly</p>
+                <p>Early</p>
+            </div>
+            <button>Go back</button>
+            <button>Next step</button>
+        </div>
     </div>
   </div>
 </template>
 
 <style>
-    .left-nav {
-  width: 274px; /* По макету из Frontend Mentor */
-  height: 568px; /* По макету */
-  background-size: cover; /* Чтобы SVG заполнило весь блок */
-  background-position: center;
-  background-repeat: no-repeat;
-  border: 2px solid red; /* Временная граница для отладки */
-}
+    .content2 h1{
+        color: #2c4158;
+        font-weight: 600;
+        margin-bottom: 15px;
+    }
+    .content2-titlep {
+        color: #888888;
+        font-size: 16px;
+        margin-bottom: 40px;
+    }
+    .select-div {
+        display: flex;
+        flex-direction: row;
+        column-gap: 20px;
+        justify-content: space-around;
+        margin-bottom: 30px;
 
+    }
+    .select-item {  
+        border: 1px solid #000;
+        border-radius: 10px;
+        width: 100%;
+        padding: 20px 20px 20px 20px;
+    }
+    .select-item h3 {
+        margin-bottom: 6px;
+    }
+    .check {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        background-color: #eefaff;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        align-items: center;
+        justify-content: center;
+    }
+    .check p {
+        font-weight: 600;
+    }
+    .select-img {
+        margin-bottom: 40px;
+    }
 </style>
